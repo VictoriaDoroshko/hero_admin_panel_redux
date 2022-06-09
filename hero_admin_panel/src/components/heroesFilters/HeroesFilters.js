8422e5402a73426b09c5d4b1004dba3fcdd2b812
Vika <viktoriav.aero@gmail.com>
2022-06-09 21:32:08 +0300

    updated filtersSlice, added extraReducers, deleted folders actions and reducers

--- hero_admin_panel/src/components/heroesFilters/HeroesFilters.js
+++ hero_admin_panel/src/components/heroesFilters/HeroesFilters.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import classNames from "classnames";
 
-import {fetchFilters} from '../../actions';
+import {fetchFilters} from '../heroesFilters/filtersSlice';
 import {activeFilterChanged} from '../heroesFilters/filtersSlice';
 import Spinner from "../spinner/Spinner";
 
--- hero_admin_panel/src/components/heroesFilters/filtersSlice.js
+++ hero_admin_panel/src/components/heroesFilters/filtersSlice.js
@@ -1,10 +1,19 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { useHttp } from '../../hooks/http.hook';
 
 const initialState = {
     filters: [],
     filtersLoadingStatus: 'idle',
     activeFilter: 'all'
 }
+
+export const fetchFilters = createAsyncThunk(
+    'filters/fetchFilters',
+    async () => {
+        const {request} = useHttp();
+        return await request('http://localhost:3001/filters');
+    }
+)
 
 const filtersSlice = createSlice({
     name: 'filters',
@@ -17,6 +26,16 @@ const filtersSlice = createSlice({
         },
         filtersFetchingError: state => {state.filtersLoadingStatus = 'error'},
         activeFilterChanged: (state, action) => {state.activeFilter = action.payload}
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchFilters.pending, state => {state.filtersLoadingStatus = 'loading'})
+            .addCase(fetchFilters.fulfilled, (state, action) => {
+                state.filtersLoadingStatus = 'idle';
+                state.filters = action.payload;
+            })
+            .addCase(fetchFilters.rejected, state => {state.filtersLoadingStatus = 'error'})
+            .addDefaultCase(() => {})
     }
 })
 
